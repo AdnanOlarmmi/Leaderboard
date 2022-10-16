d328b5d2bd08f17c63ec326ab9d2e008d951edd1
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-16 10:50:24 +0100

    Refresh Button update Msg

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,6 +1,7 @@
 import apiEndPoint from './config.js';
 
 const recentScoresEl = document.querySelector('.recent-scores__scores');
+const addMsgEl = document.querySelector('.leaderboard__add-msg');
 const renderScores = async () => {
   let markup = '';
 
@@ -14,6 +15,10 @@ const renderScores = async () => {
       markup += `<li class="leaderboard__player-scores flex-row"><span class="leaderboard__player-index flex-row">${index + 1}</span><span class="recent-scores__name">${item.user} </span><span class="recent-scores__score">${item.score}</span></li>`;
     });
     recentScoresEl.innerHTML = markup;
+    addMsgEl.innerHTML = 'Scores Updated Successfully';
+    setTimeout(() => {
+      addMsgEl.innerHTML = '';
+    }, 2000);
   } catch (err) {
     recentScoresEl.innerHTML = 'Server Down';
   }
